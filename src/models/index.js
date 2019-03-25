0cc5bf2c80b8ee13362ee83287ce6f86ad2b1382
Pablo Melendez <140300081@ucaribe.edu.mx>
2019-03-25 12:32:17 -0500

    WIP Agregar código para cargar modelos

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 import fs from 'fs';
+import path from 'path';
 import dbConfig from '../config/sequelize';
 
 const {
@@ -9,8 +10,20 @@ const {
   host,
 } = dbConfig;
 
+const db = {};
+
 const sequelize = new Sequelize(database, username, password, { host, dialect: 'mysql' });
 
 const allFiles = fs.readdirSync(__dirname);
 
 const modelFiles = allFiles.filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'));
+
+modelFiles.forEach((file) => {
+  const model = sequelize.import(path.join(__dirname, file));
+  db[model.name] = model;
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
