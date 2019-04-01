19a575ae47148ef368b5f67ab78672ced765f73b
DHCN <daniel.hcn02@gmail.com>
2019-04-01 01:23:52 -0500

    Actualizacion de modelos
    - Se setean datatypes espeficicos a atributos de modelos.
    - Se agregan comentarios a los modelos existentes.

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -6,69 +6,82 @@ module.exports = (sequelize, DataTypes) => {
 			primaryKey: true,
 			allowNull: false,
 			type: DataTypes.INTEGER,
+			comment: 'Id auto asignada a un nuevo usuario',
 		},
 
 		nombre: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(25),
+			comment: 'Nombre del usuario',
 		},
 
 		apellidos: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(40),
+			comment: 'Los apellidos del usuario',
 		},
 
 		correo: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(50),
+			comment: 'Correo electronico del usuario',
 		},
 
 		telefono: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(14),
+			comment: 'Nunero telefonico del usuario',
 		},
 
 		nombre_usuario: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(20),
+			comment: 'Nomre de usuario para logueo',
 		},
 
 		contrasena: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(15),
+			comment: 'Contraseña del usuario para logeo',
 		},
 
 		cargo_usuario: {
 			allowNull: false,
 			type: DataTypes.INTEGER,
+			comment: 'Clasificacion del usuario,capturiasta o encargado de inventario',
 		},
 
 		estado: {
 			allowNull: false,
-			type: DataTypes.STRING,
+			type: DataTypes.STRING(25),
+			comment: 'Informa el estado actual del usuario',
 		},
 
 		usuario_creo: {
 			allowNull: true,
 			type: DataTypes.INTEGER,
+			comment: 'Id del usuario que creo el registro',
 		},
 
 		fecha_creacion: {
 			allowNull: true,
 			type: DataTypes.DATE,
 			defaultValue: DataTypes.NOW,
+			comment: 'Fecha y hora de cracion del registro',
 		},
 
 		fecha_actualizacion: {
 			allowNull: true,
 			type: DataTypes.DATE,
 			defaultValue: DataTypes.NOW,
+			comment: 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
 		},
 
 		eliminado: {
 			allowNull: true,
-			type: DataTypes.INTEGER,
+			type: DataTypes.BOOLEAN,
 			defaultValue: 0,
+			comment: 'La utilidad de activar o desactivar registro',
 		}
 
 		// // Foreign Key
