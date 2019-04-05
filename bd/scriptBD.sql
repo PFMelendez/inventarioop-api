4076bbcf4c2ad6759fbdc8f607f640662476cabd
140300088@ucaribe.edu.mx <140300088@ucaribe.edu.mx>
2019-04-05 10:36:51 -0500

    Se agregaron lablas tags y objeto_tag para relacionar las tablas objetos y tags

--- bd/scriptBD.sql
+++ bd/scriptBD.sql
@@ -9,15 +9,14 @@ CREATE TABLE `cat_estados` (
   `fecha_actualizacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
   `eliminado` BIT DEFAULT 0 COMMENT 'La utilidad de activar o desactivar registro', 
   PRIMARY KEY (`id_estado`))
-  COMMENT = 'Tabla para definir el estado de un objeto bueno, regular y malo';
+COMMENT = 'Tabla para definir el estado de un objeto bueno, regular y malo';
 
 INSERT INTO cat_estados (descripcion,usuario_creo) VALUES("Bueno",1);
 INSERT INTO cat_estados (descripcion,usuario_creo) VALUES("Regular",1);
 INSERT INTO cat_estados (descripcion,usuario_creo) VALUES("Malo",1);
 
-
 CREATE TABLE `usuarios` (
-  `id_usuarios` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada a un nuevo usuario',
+  `id_usuario` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada a un nuevo usuario',
   `nombre` VARCHAR(25) NOT NULL COMMENT 'Nombre del usuario',
   `apellidos` VARCHAR(40) NOT NULL COMMENT 'Los apellidos del usuario',
   `correo` VARCHAR(50) NOT NULL COMMENT 'Correo electronico del usuario',
@@ -30,16 +29,15 @@ CREATE TABLE `usuarios` (
   `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha y hora de cracion del registro',
   `fecha_actualizacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
   `eliminado` BIT DEFAULT 0 COMMENT 'La utilidad de activar o desactivar registro',
-  PRIMARY KEY (`id_usuarios`))
+  PRIMARY KEY (`id_usuario`))
 COMMENT = 'Tabla para Guardar la informacion de los usarios del sistema';
 
 INSERT INTO usuarios(nombre,apellidos,correo,telefono,nombre_usuario,contrasena,cargo_usuario,estado,usuario_creo) VALUES("RAUL","ANDRADE ALVARADO","140300088@ucaribe.edu.mx","9981176050","RAAAUL","9982003508R",1,"ACTIVO",1);
 
-
 CREATE TABLE `objetos` (
-  `id_objetos` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada a un nuevo usuario',
-  `fecha_ingreso` DATETIME NOT NULL COMMENT 'Fecha de ingrso del objeto, fomato YYYY-MM-DD-HH:MM:SS',
-  `fecha_egreso` DATETIME NOT NULL COMMENT 'Fecha de salida del objeto, fomato YYYY-MM-DD-HH:MM:SS',
+  `id_objeto` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada a un nuevo usuario',
+  `fecha_ingreso`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha de ingrso del objeto, fomato YYYY-MM-DD-HH:MM:SS',
+  `fecha_egreso` DATETIME COMMENT 'Fecha de salida del objeto, fomato YYYY-MM-DD-HH:MM:SS',
   `lugar_hallazgo` VARCHAR(25) NOT NULL COMMENT 'Nombre del Area donde se encontro el objeto',
   `etiquetas` INT NOT NULL COMMENT 'Llave foranea para las etiquetas de caracteristicas',
   `notas` VARCHAR(100) NOT NULL COMMENT 'Datos extra sobre el objeto al entrar al inventario',
@@ -51,8 +49,32 @@ CREATE TABLE `objetos` (
   `fecha_actualizacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
   `eliminado` BIT DEFAULT 0 COMMENT 'La utilidad de activar o desactivar registro',
   FOREIGN KEY(estado) REFERENCES cat_estados(id_estado),
-  PRIMARY KEY (`id_objetos`))
+  PRIMARY KEY (`id_objeto`))
 COMMENT = 'Tabla para guardar la informacion de los objetos perdidos registrados en el sistema';
+
+CREATE TABLE `tags` (
+  `id_tag` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada cada tipo de tag creada',
+  `nombre` VARCHAR(25) NOT NULL COMMENT 'Nombre de tipo de la tag',
+  `usuario_creo` INT COMMENT 'Id del usuario que creo el registro',
+  `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha y hora de cracion del registro',
+  `fecha_actualizacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
+  `eliminado` BIT DEFAULT 0 COMMENT 'La utilidad de activar o desactivar registro',
+  PRIMARY KEY (`id_tag`))
+COMMENT = 'Tabla para agregar las tags para las busquedas de objetos';
+
+CREATE TABLE `objetos_tags` (
+  `id_objeto_tag` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada cada relacion entre tag y objeto',
+  `id_tag` INT NOT NULL COMMENT 'id de tag relacionada',
+  `id_objeto` INT NOT NULL COMMENT 'Id del objeto relacionado',
+  `usuario_creo` INT COMMENT 'Id del usuario que creo el registro',
+  `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha y hora de cracion del registro',
+  `fecha_actualizacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
+  `eliminado` BIT DEFAULT 0 COMMENT 'La utilidad de activar o desactivar registro',
+  PRIMARY KEY (`id_objeto_tag`),
+  FOREIGN KEY(id_tag) REFERENCES tags(id_tag),
+  FOREIGN KEY(id_objeto) REFERENCES objetos(id_objeto))
+COMMENT = 'Tabla para relacionar los objetos con sus tags';
+
 
 CREATE TABLE `tipo_usuario` (
   `id_tipo_usuario` INT NOT NULL AUTO_INCREMENT COMMENT 'Id auto asignada cada tipo de usuario',
@@ -65,6 +87,7 @@ CREATE TABLE `tipo_usuario` (
 COMMENT = 'Tabla para englobar los ipos de usuario';
 INSERT INTO tipo_usuario (nombre_tipo_usuario,usuario_creo) VALUES("admin",1);
 INSERT INTO tipo_usuario (nombre_tipo_usuario,usuario_creo) VALUES("capturista",1);
+
 /*Esta es la segunda parte de las tablas*/
 
 CREATE TABLE `cat_categorias` (
@@ -96,7 +119,7 @@ CREATE TABLE `cat_subcategorias` (
   `fecha_actualizacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha y hora de ultimo movimiento en la base de datos de este registro',
   `eliminado` BIT DEFAULT 0 COMMENT 'La utilidad de activar o desactivar registro', 
  PRIMARY KEY (`id_sub_categoria`),
- FOREIGN KEY(id_sub_categoria) REFERENCES cat_categorias(id_categoria))
+  FOREIGN KEY(id_sub_categoria) REFERENCES cat_categorias(id_categoria))
 COMMENT = 'Tabla para englobar los tipos de objetos por Subcategorias';
 
 CREATE TABLE `etiquetas` (
@@ -112,6 +135,5 @@ COMMENT = 'Tabla para obtener las etiquetas para busqueda de objetos';
 /*show create table objetos;*/
 
 /*Correccion de llaves foraneas de la tabla objetos*/
-
-ALTER TABLE objetos ADD FOREIGN KEY(usuario_registro_entrada) REFERENCES usuarios(id_usuarios);
-ALTER TABLE objetos ADD FOREIGN KEY(usuario_registro_salida) REFERENCES usuarios(id_usuarios);
+ALTER TABLE objetos ADD FOREIGN KEY(usuario_registro_entrada) REFERENCES usuarios(id_usuario);
+ALTER TABLE objetos ADD FOREIGN KEY(usuario_registro_salida) REFERENCES usuarios(id_usuario);
