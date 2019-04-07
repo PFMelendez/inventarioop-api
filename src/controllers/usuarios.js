fe746b2c7ca262b33e995da9e4a417251e6a9000
Pablo Melendez <140300081@ucaribe.edu.mx>
2019-04-07 00:30:59 -0500

    Crear Usuario

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -9,17 +9,21 @@ export default {
 
   async create(req, res) {
     const params = req.parsedBody;
+    // console.log(params);
 
-    try {
-      const user = await services.usuarios.create(params);
+    // try {
+    const rawUser = await services.usuarios.create(params);
+    await services.usuarios.assignType(rawUser, params.tipo_usuario);
+    const user = await services.usuarios.get(rawUser.id_usuarios);
 
-      res
-        .status(200)
-        .json({ user });
-    } catch (err) {
-      res
-        .status(err.status || 500)
-        .json({ error: err });
-    }
+
+    res
+      .status(200)
+      .json({ user });
+    // } catch (err) {
+    // res
+    //   .status(err.status || 500)
+    //   .json({ error: err });
+    // }
   },
 };
--- src/services/usuarios.js
+++ src/services/usuarios.js
@@ -2,15 +2,22 @@ import Models from '../models';
 
 export default {
   create: async (params) => {
-    console.log('Llega');
     const {
-      nombre, apellidos, correo, nombre_usuario, constrasena,
+      nombre, apellidos, correo, nombre_usuario, contrasena,
     } = params;
 
-    if (!nombre || !apellidos || !correo || !nombre_usuario || !constrasena) {
+    if (!nombre || !apellidos || !correo || !nombre_usuario || !contrasena) {
       throw new Error({ status: 400, message: 'Bad Request. Missing Fields' });
     }
 
-    return Models.Usuario.create(params);
+    return Models.Usuario.create({
+      nombre, apellidos, correo, nombre_usuario, contrasena,
+    });
   },
+  assignType: async (user, type) => user.setTipoUsuario(type),
+  get: async userId => Models.Usuario.findByPk(userId, {
+    include: [
+      { all: true },
+    ],
+  }),
 };
