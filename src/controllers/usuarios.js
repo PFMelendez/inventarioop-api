581a64b245c32cef1c8ff4dd99c8a36d4e34c1c1
Pablo Melendez <140300081@ucaribe.edu.mx>
2019-04-06 23:11:24 -0500

    WIP

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,7 +1,25 @@
+import services from '../services';
+
 export default {
   hi(req, res) {
     res.status(200).json({
       hello: 'world',
     });
+  },
+
+  async create(req, res) {
+    const params = req.parsedBody;
+
+    try {
+      const user = await services.usuarios.create(params);
+
+      res
+        .status(200)
+        .json({ user });
+    } catch (err) {
+      res
+        .status(err.status || 500)
+        .json({ error: err });
+    }
   },
 };
--- src/services/usuarios.js
+++ src/services/usuarios.js
@@ -1,6 +1,16 @@
 import Models from '../models';
 
 export default {
-  create: async params => Models.Usuario.create(params),
-};
+  create: async (params) => {
+    console.log('Llega');
+    const {
+      nombre, apellidos, correo, nombre_usuario, constrasena,
+    } = params;
 
+    if (!nombre || !apellidos || !correo || !nombre_usuario || !constrasena) {
+      throw new Error({ status: 400, message: 'Bad Request. Missing Fields' });
+    }
+
+    return Models.Usuario.create(params);
+  },
+};
